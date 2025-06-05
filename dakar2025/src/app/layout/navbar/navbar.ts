dfa41fed0269a8a7dfe60e6b28ffa46459049653
Jakub Klewicki <kuba.klewicki@gmail.com>
2025-06-05 19:31:34 +0200

    Major update 05-06-2025: - finish all pages - deploy

--- dakar2025/src/app/layout/navbar/navbar.ts
+++ dakar2025/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common'; // Import ViewportScroller
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  // Pamiętaj o dodaniu ViewportScroller do imports, jeśli używasz standalone component (co sugeruje Angular 20)
+  // W przeciwnym razie, jeśli to moduł, dodaj CommonModule do imports modułu
+  imports: [], // Tutaj powinieneś mieć CommonModule, jeśli to moduł, lub po prostu jest to standalone i nie wymaga jawnego importu dla ViewportScroller
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller // Wstrzyknij ViewportScroller
+  ) {}
 
+  scrollTo(fragment: string) {
+    // Jeśli nie jesteś na stronie home, przejdź do niej z fragmentem
+    // Po nawigacji, lub jeśli jesteś już na stronie home, przewiń do kotwicy
+    if (this.router.url !== '/home') {
+      // Jeśli jesteś na innej stronie, nawiguj do home z fragmentem
+      this.router.navigate(['/home'], { fragment: fragment }).then(() => {
+        // Po nawigacji, przewiń do kotwicy.
+        // Należy to zrobić w bloku .then() aby upewnić się, że DOM jest już gotowy.
+        this.viewportScroller.scrollToAnchor(fragment);
+      });
+    } else {
+      // Jeśli jesteś już na stronie home, po prostu przewiń do kotwicy
+      this.viewportScroller.scrollToAnchor(fragment);
+    }
+  }
 }
--- dakar2025/src/app/pages/home/home.ts
+++ dakar2025/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Navbar } from "../../layout/navbar/navbar";
 import { Footer } from "../../layout/footer/footer";
 import { Slider } from '../../shared/slider/slider';
@@ -11,6 +12,17 @@ import { Slider } from '../../shared/slider/slider';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComoponent {
+export class HomeComoponent implements AfterViewInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const el = document.getElementById(fragment);
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    });
+  }
 }
