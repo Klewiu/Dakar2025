61f8d32870c95da31d0aa9ceeb84b4b883aea2b7
Jakub Klewicki <kuba.klewicki@gmail.com>
2025-06-12 17:10:10 +0200

    UPDATE - 12.06.2025: - add AOS animation - new pics - contact - new phone and emial

--- dakar2025/src/app/pages/home/home.ts
+++ dakar2025/src/app/pages/home/home.ts
@@ -1,9 +1,11 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Navbar } from "../../layout/navbar/navbar";
 import { Footer } from "../../layout/footer/footer";
 import { Slider } from '../../shared/slider/slider';
-
+// @ts-ignore – jeśli masz błąd braku typów
+import * as AOS from 'aos';
+import 'aos/dist/aos.css'; // Upewnij się, że styl AOS jest zaimportowany
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,15 @@ import { Slider } from '../../shared/slider/slider';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComoponent implements AfterViewInit {
+export class HomeComoponent implements OnInit, AfterViewInit { // <-- literówka w nazwie klasy
   constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1500,
+      once: false, // animacje tylko raz przy wejściu
+    });
+  }
 
   ngAfterViewInit(): void {
     this.route.fragment.subscribe(fragment => {
